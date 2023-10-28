4470f857608aacc38aefd6bb72ad6262dc52d1f1
lavanyaibex <lavanya.govada@ibexlabs.com>
2023-10-28 20:44:31 +0530

    added backend

--- environments/prod-env/config.tf
+++ environments/prod-env/config.tf
@@ -11,3 +11,12 @@ terraform {
 provider "aws" {
   region = "us-east-1" # define region as per your account
 }
+
+#backend state
+terraform {
+  backend "s3" {
+    bucket         = "lavanya-terraform-statefile-dontdelete" # Use the same bucket name as defined in main.tf
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1" # Set your AWS region
+  }
+}

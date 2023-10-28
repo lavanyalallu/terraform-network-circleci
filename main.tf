a8c657624580e84f027ed402e17c83c555793922
lavanyaibex <lavanya.govada@ibexlabs.com>
2023-10-28 18:05:48 +0530

    added provider

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.12.1"
+    }
+  }
+}
 variable "name" { default = "main" }
 variable "cidr" { default = "10.139.0.0/16" }
 
@@ -11,7 +23,4 @@ module "network" {
 output "env"      { value = "${var.name}" }
 output "vpc_cidr" { value = "${module.network.vpc_cidr}" }
 
-provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
-}
+

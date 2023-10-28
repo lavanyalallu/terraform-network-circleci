23297c978e01011fd08ac566075fe61f1a235d5a
lavanyalallu <96727676+lavanyalallu@users.noreply.github.com>
2023-10-28 14:15:16 +0530

    Update network.tf

--- modules/network.tf
+++ modules/network.tf
@@ -6,9 +6,9 @@ resource "aws_vpc" "vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags {
-    Name = "${var.name}"
-  }
+  #tags {
+    #Name = "${var.name}"
+  #}
 }
 
 resource "aws_security_group" "allow" {

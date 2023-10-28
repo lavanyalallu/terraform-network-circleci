b471daae059b8fca797826afa3fc363bfcd85362
lavanyalallu <96727676+lavanyalallu@users.noreply.github.com>
2023-10-28 14:01:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ variable "name" { default = "main" }
 variable "cidr" { default = "10.139.0.0/16" }
 
 module "network" {
-  source = "https://github.com/lavanyalallu/terraform-network-circleci/blob/main/modules/network.tf"
+  source = ".//network"
 
   name = "${var.name}"
   cidr = "${var.cidr}"

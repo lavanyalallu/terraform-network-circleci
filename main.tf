26cd725e12f905ff9d0e17c23d28f9767091ff5d
lavanyalallu <96727676+lavanyalallu@users.noreply.github.com>
2023-10-28 14:07:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ variable "name" { default = "main" }
 variable "cidr" { default = "10.139.0.0/16" }
 
 module "network" {
-  source = ".//network"
+  source = ".//modules"
 
   name = "${var.name}"
   cidr = "${var.cidr}"
